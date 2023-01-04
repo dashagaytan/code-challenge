a40d1112ebc7220f9146910d11c7f32baa91ea87
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-01-04 16:52:27 -0600

    setup

---
+++ src/Form.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+function Form(props) {
+    const [form, setForm] = useState({firstname: '', lastName: ''})
+
+  const handleChange = (e) => {
+    
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="First Name"
+        name=""
+        value=""
+        onChange={handleChange}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default Form;
